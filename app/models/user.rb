3e40eb8c6e790f853405daa0aff78873b22bb62d
ethanryan <ethanryan@gmail.com>
2017-12-12 08:56:18 -0500

    adding validates uniqueness of name to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :stories
 
-  validates :name,  presence: true
-  validates :password,  presence: true #adding this cuz of rspec test
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true #adding this cuz of rspec test
 
 end
