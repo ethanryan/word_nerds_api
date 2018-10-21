b5c51deb9d7dd0fd01dc67e219f63e2a4046828d
ethanryan <ethanryan@gmail.com>
2018-10-21 15:09:00 -0400

    ran rake db migrate, updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181021014331) do
+ActiveRecord::Schema.define(version: 20181021171655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
